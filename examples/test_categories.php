3d61b43e2d7bf45e1cda1d33380032c8bd62764d
Niklas Nilsson <splitfeed@gmail.com>
2010-06-22 14:08:52 +0000

    Added categories example

---
+++ examples/test_categories.php
@@ -0,0 +1,31 @@
+<?php
+include("../local.php");
+include("../proto/protocolbuffers.inc.php");
+include("../proto/market.proto.php");
+include("../Market/MarketSession.php");
+
+$session = new MarketSession();
+$session->login(GOOGLE_EMAIL, GOOGLE_PASSWD);
+
+$cr = new CategoriesRequest();
+
+$reqGroup = new Request_RequestGroup();
+$reqGroup->setCategoriesRequest($cr);
+$response = $session->execute($reqGroup);
+
+$groups = $response->getResponsegroupArray();
+foreach ($groups as $rg) {
+	$categoriesResponse = $rg->getCategoriesResponse();
+	$categories = $categoriesResponse->getCategoriesArray();
+	foreach ($categories as $category) {
+		echo $category->getTitle()."<br/>";
+
+		$subcategories = $category->getSubCategoriesArray();
+		foreach ($subcategories as $subcategory) {
+			echo "- ".$subcategory->getTitle()."<br/>";
+		}
+	}
+
+
+
+}
